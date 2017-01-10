1b072708620c5b8a21ec2be96c8a3a74226c5a00
Cassie <cassie.pericak@northwestern.edu>
2017-01-09 20:02:20 -0600

    created two seperate servers

--- creating_servers.js
+++ creating_servers.js
@@ -1,9 +1,34 @@
-// * Using the previous example as a guide, create an app that has two web servers.*One that listens on port 7000 and one that listens on port 7500. * The one listening on port 7000 will always tell the user something good about themselves.*The one listening on 7500 will always tell the user something bad about themselves.*Make sure you create a Github repo and commit this code!
+// * Using the previous example as a guide, create an app that has two web servers.*One that listens on port 7000 and one that listens on port 7500. 
+//* The one listening on port 7000 will always tell the user something good about themselves.*The one listening on 7500 will always tell the user something bad about themselves.
+//*Make sure you create a Github repo and commit this code!
 
-    //     ** Bonus **
-    //     * Look
-    // for other ways to expand what your server can do .As possibilities:
-    //     * Generate the good / bad phrase randomly from a list of predefined phrases * Use the `twitter`
-    // package
-    // inside the response to also
-    // return a random tweet
+//require http module
+var http = require("http");
+
+//define the ports to listen to
+var port1 = 7000;
+var port2 = 7500;
+
+var server1 = http.createServer(function(request, response) {
+    response.end("you are awesome: " + request.url);
+});
+
+server1.listen(port1, function() {
+    console.log("server1 listening on: http://localhost:%s", port1);
+});
+
+var server2 = http.createServer(function(request, response) {
+    response.end("you suck: " + request.url);
+});
+
+server2.listen(port2, function() {
+    console.log("server2 listening on: http://localhost:%s", port2);
+});
+
+//     ** Bonus **
+//     * Look
+// for other ways to expand what your server can do .As possibilities:
+//     * Generate the good / bad phrase randomly from a list of predefined phrases * Use the `twitter`
+// package
+// inside the response to also
+// return a random tweet
